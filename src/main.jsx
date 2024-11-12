3efe5c90e746e7e9c4488b53255c36507569bd7c
JoakoSavini <savinijoaquin@gmail.com>
2024-11-12 19:49:53 -0300

    cambio de theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'primeicons/primeicons.css'
-import 'primereact/resources/themes/md-dark-deeppurple/theme.css'
+import 'primereact/resources/themes/vela-purple/theme.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
